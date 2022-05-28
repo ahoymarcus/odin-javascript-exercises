2fc82f98690fcd9b06c34d2b84c714fbe39b1ef9
Marcus Vinicius Richa <marcus.richa@protonmail.com>
2022-05-28 13:01:51 -0300

    Complete test 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,33 @@
-const ftoc = function() {
-
+const ftoc = function(tempFar) {
+    let tempCels = 0;
+    
+    tempCels = (tempFar - 32) * (5 / 9);
+    
+    return Number(tempCels.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(tempCels) {
+    let tempFar = 0;
+    
+    tempFar = (tempCels * 9 / 5) + 32;
+    
+    return Number(tempFar.toFixed(1));
 };
+
+
+
+
+// ftoc(tempFar)
+ftoc(32)
+// ftoc(100)
+// ftoc(-100)
+
+// ctof(tempCels)
+ctof(0)
+// ctof(73,2)
+// ctof(-10)
+
+
 
 // Do not edit below this line
 module.exports = {
