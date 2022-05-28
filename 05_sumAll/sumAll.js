499f70b678c6c2a0eda45fc9e0762de9b54e2fea
Marcus Vinicius Richa <marcus.richa@protonmail.com>
2022-05-28 11:32:21 -0300

    Complete test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,47 @@
-const sumAll = function() {
+const sumAll = function(...nums) {
+    //console.log(nums);
+    
 
+    
+    let total = 0;
+    
+    if (
+            Number.isInteger(nums[0])
+        && Number.isInteger(nums[nums.length -1])
+        && nums[0] >= 0
+        && nums[0] < nums[nums.length -1]
+    ) {
+        for (let i = 0; i <= nums[nums.length - 1]; i += 1) {
+            //console.log(i);
+            
+            total += i;
+        }
+    } else if (
+            Number.isInteger(nums[0])
+        && Number.isInteger(nums[nums.length - 1])
+        && nums[0] > 0
+    ){
+        for (let i = nums[0]; i >= nums[nums.length - 1]; i -= 1) {
+            //console.log(i);
+            
+            total += i;
+        }
+    } else {
+        return "ERROR";
+    }
+    
+    
+    
+    return total;
 };
+
+
+//console.log(sumAll());
+//console.log(sumAll(1, 4));
+//console.log(sumAll(123, 1));
+console.log(sumAll(-10, 4));
+//console.log(sumAll());
+
 
 // Do not edit below this line
 module.exports = sumAll;
