f28269e9030a2e4c5fd01ce5f3dfca2027148348
Marcus Vinicius Richa <marcus.richa@protonmail.com>
2022-05-27 22:35:31 -0300

    Install Jest. Complete tests 1 to 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,25 @@
-const removeFromArray = function() {
+const arr1 = [1, 2, 3, 4];
 
+//const args = '3, 2';
+// ["hey", 2, 3, "ho"].sort(function(a, b) { return a - b });
+
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+    
+    //args.sort((a, b) => a - b);
+    //console.log(args);
+    
+    // includes() is case sensitive and doesn't convert
+    result = arr.filter(item => !args.includes(item));
+    
+    return result;
 };
+
+
+//console.log(removeFromArray(arr1, 3, 2));
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
