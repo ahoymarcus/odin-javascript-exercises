0fe16627b3ea8db434187fd0a76483de5c05311f
Marcus Vinicius Richa <marcus.richa@protonmail.com>
2022-06-03 20:35:58 -0300

    Finalize test 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return 'OOPS';
+    }
+    
+    const fibSerie = [1, 1];
+    
+    for (let i = 2; i < num; i += 1) {
+        fibSerie[i] = fibSerie[i - 1] + fibSerie[i - 2];
+    }
+    
+    console.log(fibSerie);
+    
+    return fibSerie[fibSerie.length - 1];
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+
+console.log(fibonacci(4));
+// console.log(fibonacci(10));
+// console.log(fibonacci(-25));
+// console.log(fibonacci('1'));
+// console.log(fibonacci('8'));
+
+
