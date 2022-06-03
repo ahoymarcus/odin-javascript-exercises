4662a05710b8136cf5902e2cabab849e2d3c29f5
Marcus Vinicius Richa <marcus.richa@protonmail.com>
2022-06-03 12:19:32 -0300

    Finalize test 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const result = arr.reduce((acc, cur) => acc += cur, 0);
+    
+    return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    const result = arr.reduce((acc, cur) => acc *= cur, 1);
+    
+    return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) {
+        return 'Please, give a positive number!';
+    } 
+    
+    if (num === 0) {
+        return 1;
+    }
+    
+    return num * factorial(num - 1);
 };
 
 // Do not edit below this line
@@ -31,3 +43,16 @@ module.exports = {
   power,
   factorial
 };
+
+
+// console.log(factorial(-5));
+// console.log(factorial(0));
+// console.log(factorial(1));
+// console.log(factorial(2));
+console.log(factorial(5));
+//console.log(factorial(10));
+
+
+
+
+
