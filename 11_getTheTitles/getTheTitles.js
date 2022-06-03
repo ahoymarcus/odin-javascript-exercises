1c4f478f74a5d87c4d66ca5993107a668d98c455
Marcus Vinicius Richa <marcus.richa@protonmail.com>
2022-06-03 20:44:11 -0300

    Finalize test 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
+const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ];
 
+
+const getTheTitles = function(arr) {
+    const result = arr.map(cur => cur.title);
+    
+    return result;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+
+
+console.log(getTheTitles(books));
+
